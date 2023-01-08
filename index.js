2c71007163d71c51df8e55f92c003bdabc2dcdba
wanna-be-pro <work.mahmudulhasan@gmail.com>
2023-01-08 21:09:36 +0600

    successfull to connect with fontend

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ const express = require("express");
 var bodyParser = require("body-parser");
 const app = express();
 const jsonParser = bodyParser.json();
-
-var urlencodedParser = bodyParser.urlencoded({ extended: false });
+const cors = require("cors")
+app.use(cors())
+// var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 app.get("/", (req, res) => {
   res.send("server is running......");
@@ -16,42 +17,44 @@ app.get("/sendEmail", (req, res) => {
   res.send("hitted");
 });
 app.post("/sendEmail", jsonParser, (req, res) => {
+  
+  console.log("hitted to sendEmail");
+  console.log(req.body)
+
   const email = req.body.email;
   const name = req.body.firstName + " " + req.body.lastName;
   const number = req.body.number;
-  const msg = `Hi, this is ${name} 
+  const msg = `
+  Hi, this is ${name} 
    ${req.body.message}
-
-
    contact info:
    phone: ${number}
    email: ${email}
    `;
   const path = req.body.file;
 
-  const tranporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-      user: "dcoders00@gmail.com",
-      pass: "mfhnemtnjingnfkg",
-    },
-  });
+  // const tranporter = nodemailer.createTransport({
+  //   service: "gmail",
+  //   auth: {
+  //     user: "dcoders00@gmail.com",
+  //     pass: "mfhnemtnjingnfkg",
+  //   },
+  // });
 
-  const mailOption = {
-    from: "dcoders00@gmail.com",
-    to: "tahmimaahmed22@gmail.com",
-    subject: "mail from form",
-    text: msg,
-    attachments: [{ path: path }],
-  };
-  tranporter.sendMail(mailOption, (err, info) => {
-    if (err) {
-      console.log(err);
-    } else {
-      console.log("email sent " + info.response);
-    }
-  });
-
+  // const mailOption = {
+  //   from: "dcoders00@gmail.com",
+  //   to: "tahmimaahmed22@gmail.com",
+  //   subject: "mail from form",
+  //   text: msg,
+  //   // attachments: [{ path: path }],
+  // };
+  // tranporter.sendMail(mailOption, (err, info) => {
+  //   if (err) {
+  //     console.log(err);
+  //   } else {
+  //     console.log("email sent " + info.response);
+  //   }
+  // });
   res.send(path);
 });
 
